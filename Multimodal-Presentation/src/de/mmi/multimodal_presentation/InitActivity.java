ba38add3f62aada02f4fc624c99545f6afd6fed9
malaka <pat.eng@hotmail.com>
2014-05-26 14:49:15 +0200

    added missing files

--- Multimodal-Presentation/src/de/mmi/multimodal_presentation/InitActivity.java
+++ Multimodal-Presentation/src/de/mmi/multimodal_presentation/InitActivity.java
@@ -1,10 +1,13 @@
 package de.mmi.multimodal_presentation;
 
+import de.mmi.multimodal_presentation.network.ConnectionService;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class InitActivity extends Activity {
 
@@ -18,6 +21,8 @@ public class InitActivity extends Activity {
          */
         Button presentButton = (Button) findViewById(R.id.start_present_activity);
         Button gyroButton = (Button) findViewById(R.id.start_gyro_activity);
+        Button scanButton = (Button) findViewById(R.id.scan_ip);
+        Button exitButton = (Button) findViewById(R.id.exit);
         
         /*
          * Add onclicklisteners to start corresponding activity 
@@ -37,5 +42,45 @@ public class InitActivity extends Activity {
 				startActivity(actInt);
 			}
 		});
+        
+        scanButton.setOnClickListener(new View.OnClickListener() {	
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, 0);		
+			}
+		});
+        
+        exitButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), ConnectionService.class);
+				intent.setAction(ConnectionService.EXIT);
+				startService(intent);
+			}
+		});
+        //startService()
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+    	if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                //String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Log.i("InitAct", "Scanned ip: " + contents);
+                
+                Intent i = new Intent(getApplicationContext(), ConnectionService.class);
+                i.setAction(ConnectionService.CONNECT);
+                i.putExtra(ConnectionService.IP, contents);
+                startService(i);
+                // Handle successful scan
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            	Toast.makeText(getApplicationContext(), "Scan not successful", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- Multimodal-Presentation/src/de/mmi/multimodal_presentation/PresentActivity.java
+++ Multimodal-Presentation/src/de/mmi/multimodal_presentation/PresentActivity.java
@@ -1,19 +1,151 @@
 package de.mmi.multimodal_presentation;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 
-public class PresentActivity extends Activity {
+import com.qoppa.android.pdfViewer.fonts.StandardFontTF;
+import com.qoppa.viewer.QPDFViewerView;
 
-	private final static String TAG = "presenter";
+import de.mmi.multimodal_presentation.network.ConnectionService;
+import de.mmi.multimodal_presentation.network.MessageSet;
+
+public class PresentActivity extends Activity implements GestureDetector.OnGestureListener, OnTouchListener{
+
+	//private final static String TAG = "presenter";
+	private QPDFViewerView pdfView;
+    private View gestureCatcher;
+    private GestureDetector mGestureDetector;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_present);
 		
+		StandardFontTF.mAssetMgr = getAssets();
+		
+		mGestureDetector = new GestureDetector(this, this);
+		
+		gestureCatcher = findViewById(R.id.gesture_catcher);
+		gestureCatcher.setOnTouchListener(this);
+		
+		pdfView = (QPDFViewerView) findViewById(R.id.main_reader);
+		/*				
+		ViewGroup parent = (ViewGroup) pdfView.getParent();
+		int index = parent.indexOfChild(pdfView);
+		parent.removeView(pdfView);
+		pdfView = new QPDFViewerView(this);
+		parent.addView(pdfView, index);		
+		*/
+		
+		pdfView.setActivity(this);
+
+		pdfView.open();
+		pdfView.getToolbar().setVisibility(View.GONE);
+		
+		pdfView.setMaximumScale(100f);
+		pdfView.setTouchListenerActive(false);
+		
+        
 		// Don't fall asleep as we need to access screen the whole time while presenting
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	  super.onConfigurationChanged(newConfig);
+	  pdfView.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		this.mGestureDetector.onTouchEvent(event);
+		return true;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		float x = e.getX();
+		float y = e.getY();
+		
+		//float relativeX = 0f;
+		//float relativeY = 0f;
+		
+		View pageView = pdfView.getPageView(pdfView.getCurrentPageNumber());
+		
+		//int top = pageView.getTop();
+		//int bottom = pageView.getBottom();
+		
+		/*if(top != pdfView.getTop()){
+			
+		}else if(bottom != pdfView.getBottom()){
+			
+		}*/
+		
+		Intent i = new Intent(getApplicationContext(), ConnectionService.class);
+		i.setAction(ConnectionService.HIGHLIGHT);
+		i.putExtra("X", x / (float)pageView.getWidth());
+		i.putExtra("Y", y / (float)pageView.getHeight());
+		
+		startService(i);
+		
+		return true;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		
+		String command;
+		
+		if(velocityX < 0f){
+			command = MessageSet.NEXT;
+			pdfView.nextPage();
+		}else{
+			command = MessageSet.PREVIOUS;
+			pdfView.prevPage();
+		}
+		
+		pdfView.getPageView(pdfView.getCurrentPageNumber()-1).invalidate();
+		
+		Intent i = new Intent(getApplicationContext(), ConnectionService.class);
+		i.setAction(ConnectionService.COMMAND);
+		i.putExtra(ConnectionService.COMMAND, command);
+		
+		startService(i);
+		
+		return true;
+	}
+
 }
